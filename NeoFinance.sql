a45b64bb4dd8bba59ebe1d781e33f3031642eb93
Caio Tavares <137078120+caioviny@users.noreply.github.com>
2024-10-17 09:32:45 -0300

    bd update metas

--- NeoFinance.sql
+++ NeoFinance.sql
@@ -1,3 +1,4 @@
+-- Criação do banco de dados
 CREATE DATABASE finance;
 
 USE finance;
@@ -6,10 +7,13 @@ USE finance;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    email VARCHAR(191) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    remember_token VARCHAR(64), -- Coluna remember_token adicionada
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    saldo DECIMAL(10, 2) DEFAULT 0.00,  -- Campo para armazenar o saldo do usuário
+    saldo_inicial_adicionado TINYINT(1) DEFAULT 0,  -- Para marcar se o saldo inicial foi adicionado
+    remember_token VARCHAR(64) DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- Criação da tabela para armazenar tentativas de login
@@ -33,47 +37,13 @@ CREATE TABLE password_resets (
 -- Criação da tabela de categorias vinculada ao usuário
 CREATE TABLE categorias (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT NOT NULL, -- Relacionamento com a tabela de usuários
+    usuario_id INT NOT NULL,
     nome VARCHAR(191) NOT NULL,
-    icone VARCHAR(255) NOT NULL, -- Armazena a classe do ícone
+    icone VARCHAR(255) NOT NULL,
+    excluida BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (usuario_id) REFERENCES users (id) ON DELETE CASCADE,
-    UNIQUE (usuario_id, nome) -- Garante que um usuário não crie categorias duplicadas
+    UNIQUE (usuario_id, nome) -- Garante que um usuário não crie categorias duplicadas apenas se não estiver excluída
 );
-
-
--- Comando pra deletar categorias já existentes
-DELETE FROM categorias;
-
--- Inserção de categorias predefinidas para todos os usuários
-INSERT INTO categorias (usuario_id, nome, icone)
-SELECT u.id, tmp.nome, tmp.icone
-FROM users u  
-JOIN (
-    SELECT 'Moradia' AS nome, 'fi-sr-home' AS icone UNION ALL
-    SELECT 'Beleza', 'fi-br-scissors' UNION ALL
-    SELECT 'Telefone', 'fi-br-smartphone' UNION ALL
-    SELECT 'Fatura', 'fi-sr-file-invoice-dollar' UNION ALL
-    SELECT 'Transferência', 'fi-br-money-coin-transfer' UNION ALL
-    SELECT 'Viagem Aérea', 'fi-ss-plane-alt' UNION ALL
-    SELECT 'Viagem de Ônibus', 'fi-ss-bus-alt' UNION ALL
-    SELECT 'Ferramenta', 'fi-ss-wrench-alt' UNION ALL
-    SELECT 'Mecânica', 'fi-ss-car-mechanic' UNION ALL
-    SELECT 'Supermercado', 'fi-sr-shopping-cart' UNION ALL
-    SELECT 'Carteira', 'fi-sr-wallet' UNION ALL
-    SELECT 'Videogame', 'fi-sr-gamepad' UNION ALL
-    SELECT 'Fast Food', 'fi-ss-hotdog' UNION ALL
-    SELECT 'Médico', 'fi-sr-user-md' UNION ALL
-    SELECT 'Animal - Cão', 'fi-sr-dog-leashed' UNION ALL
-    SELECT 'Animal - Brinquedos', 'fi-sr-bone' UNION ALL
-    SELECT 'Animal - Gato', 'fi-sr-cat' UNION ALL
-    SELECT 'Computador', 'fi-sr-devices' UNION ALL
-    SELECT 'Livro', 'fi-ss-book-alt'
-) AS tmp
-WHERE NOT EXISTS (
-    SELECT 1 FROM categorias c WHERE c.usuario_id = u.id AND c.nome = tmp.nome
-);
-
-
 -- Criação da tabela de transações
 CREATE TABLE transacoes (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -88,6 +58,7 @@ CREATE TABLE transacoes (
     FOREIGN KEY (categoria_id) REFERENCES categorias (id) ON DELETE CASCADE,
     icone VARCHAR(255) -- Pode armazenar a classe do ícone, se necessário
 );
+
 
 -- Criação da tabela de entradas no calendário
 CREATE TABLE entradas_calendario (
@@ -126,9 +97,53 @@ CREATE TABLE vencimentos (
     descricao VARCHAR(255),
     data_vencimento DATE,
     valor DECIMAL(10, 2),
+    tipo_transacao ENUM('Receita', 'Despesa') NOT NULL,
     categoria VARCHAR(50),
     status VARCHAR(20),
     FOREIGN KEY (usuario_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
+-- Criação da tabela de Metas
+CREATE TABLE metas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    nome_meta VARCHAR(255) NOT NULL,
+    valor_alvo DECIMAL(10, 2) NOT NULL,
+    valor_atual DECIMAL(10, 2) DEFAULT 0.00,  -- Valor atual economizado
+    data_limite DATE,  -- Data limite para atingir a meta
+    criada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES users (id) ON DELETE CASCADE
+);
 
+-- Criação do Trigger para inserir categorias predefinidas para novos usuários
+DELIMITER //
+
+CREATE TRIGGER after_user_insert
+AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+    INSERT INTO categorias (usuario_id, nome, icone)
+    VALUES
+        (NEW.id, 'Moradia', 'fi-sr-home'),
+        (NEW.id, 'Beleza', 'fi-br-scissors'),
+        (NEW.id, 'Telefone', 'fi-br-smartphone'),
+        (NEW.id, 'Fatura', 'fi-sr-file-invoice-dollar'),
+        (NEW.id, 'Transferência', 'fi-br-money-coin-transfer'),
+        (NEW.id, 'Viagem Aérea', 'fi-ss-plane-alt'),
+        (NEW.id, 'Viagem de Ônibus', 'fi-ss-bus-alt'),
+        (NEW.id, 'Ferramenta', 'fi-ss-wrench-alt'),
+        (NEW.id, 'Mecânica', 'fi-ss-car-mechanic'),
+        (NEW.id, 'Supermercado', 'fi-sr-shopping-cart'),
+        (NEW.id, 'Carteira', 'fi-sr-wallet'),
+        (NEW.id, 'Videogame', 'fi-sr-gamepad'),
+        (NEW.id, 'Fast Food', 'fi-ss-hotdog'),
+        (NEW.id, 'Médico', 'fi-sr-user-md'),
+        (NEW.id, 'Animal - Cão', 'fi-sr-dog-leashed'),
+        (NEW.id, 'Animal - Brinquedos', 'fi-sr-bone'),
+        (NEW.id, 'Animal - Gato', 'fi-sr-cat'),
+        (NEW.id, 'Computador', 'fi-sr-devices'),
+        (NEW.id, 'Livro', 'fi-ss-book-alt');
+END;
+//
+
+DELIMITER ;
